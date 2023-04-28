c977e9f7b2476291dedf6159cefe51e66f9eb7e8
lazare1999 <47102192+lazare1999@users.noreply.github.com>
2023-04-28 20:54:04 +0400

    123
    123

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
-import { Button, SafeAreaView, Platform, StatusBar, StyleSheet, Text, View } from 'react-native';
-import MyAxiosFunctions from './axiosFunctions';
+import { Button, SafeAreaView, Platform, StatusBar, StyleSheet, Text, View, Pressable } from 'react-native';
+import MyAxiosFunctions from './axiosFunctions/axiosFunctions';
 
 const myAxiosFunctions = new MyAxiosFunctions()
 
 export default function App() {
 
   const [count, setCount] =useState(0) 
-  const [data, setData] =useState(null)
+  const [data, setData] =useState("ნუ გამასუმაშეცეეეთ")
 
   useEffect(()=>{
 
@@ -34,10 +34,43 @@ export default function App() {
         <Button title={count.toString()} onPress={()=>{
           setCount(count+1)
         }}/>
+        <Pressable style={styles.btn} onPress={()=>{
+          console.log('aaa')
+        }}>
+          <Text style={styles.txt}>Press Me</Text>
+        </Pressable>
         <Text>{JSON.stringify(data)}</Text>
       </View>
 
     </SafeAreaView>
-   
   );
+
+
 }
+
+const styles = StyleSheet.create({
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  btn:{
+    backgroundColor:'blue',
+    padding:10,
+    borderRadius:30,
+    shadowColor: "black",
+    shadowOffset: {
+    width: 0,
+    height: 8,
+    shadowOpacity: 0.46,
+    shadowRadius: 11.14,
+  
+    elevation: 17,
+    },
+    txt:{
+      color:'white',
+    }
+  },
+
+})
